ad979907cf06c9e7d7dbf0bebe3de34a9b5b8dfd
Marco Antônio Almeida <marco.almeida@sou.unaerp.edu.br>
2023-07-18 00:35:00 -0300

    firebase configuration

--- managementapp/lib/main.dart
+++ managementapp/lib/main.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:managementapp/firebase_options.dart';
 import 'View/main_view.dart';
 
 // Main function
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
